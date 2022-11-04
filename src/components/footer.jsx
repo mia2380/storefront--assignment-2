07d17d81f5fe7326963915d77290ba093b938ca6
mia2380 <112995643+mia2380@users.noreply.github.com>
2022-11-03 22:50:48 -0400

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+
+function Footer(){
+    return (
+        <div className="footer">
+            <div>2022 </div>
+            <div className="name"> Brenda Allemand</div>
+        </div>
+    );
+}
+
+export default Footer;
